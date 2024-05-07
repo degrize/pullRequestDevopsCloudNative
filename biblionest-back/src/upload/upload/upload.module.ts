c740dd7588239ff68a7e472087c8eecdb027611c
luis-borges <kdegrize@gmail.com>
2024-05-07 19:01:30 +0200

    suppression all files

--- biblionest-back/src/upload/upload/upload.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UploadsController } from './upload.controller';
-import { UploadService } from './upload.service';
-
-@Module({
-  controllers: [UploadsController],
-  providers: [UploadService]
-})
-export class UploadModule {}
